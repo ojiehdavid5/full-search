d09dfb54456ddd12ffd520d9b0b8b599a8439133
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-17 19:27:17 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express =require('express');
+
+// import {User} from './user'
+
+const { Users }=require('./user');
+
+const app=express();
+
+console.log(Users);
+
+
+const cors=require('cors');
+
+app.use(cors());
+
+
+app.get('/',(req,res)=>{
+res.json(Users);
+
+})
+
+
+app.listen(5000,()=> console.log("API IS WORKING"));
